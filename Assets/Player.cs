c9bb90dcda0bc690884be165675f53f9852fb9a7
KoastQ <60191935+KoastQ@users.noreply.github.com>
2020-01-26 20:46:30 -0600

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1.5f;
+    private Vector2 velocity = Vector2.zero;
+    private Vector2 clickPos;   //to store the mouse position when clicked
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clickPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        moveToMouse();
+    }
+
+    void move()
+    {
+        Vector2 dir = Vector2.zero;
+
+        //Reads keys
+        if (Input.GetKey(KeyCode.D))
+        {
+            dir += Vector2.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir += Vector2.up;
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            dir += Vector2.down;
+
+        }
+
+        velocity = dir.normalized * speed;
+        //Apply the change
+        transform.position = transform.position + (Vector3)(velocity* Time.deltaTime);
+    }
+
+    void moveToMouse()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+            clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector3.MoveTowards(transform.position, clickPos,10*(speed*Time.deltaTime));
+
+        }
+    }
+
+}
